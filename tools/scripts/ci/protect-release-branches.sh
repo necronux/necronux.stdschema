0318cf3e8f9e060211e570ea18170775c2136d81
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2025-05-14 21:11:56 +0530

    fix branch prot scripts to accurately target branches

--- tools/scripts/ci/protect-release-branches.sh
+++ tools/scripts/ci/protect-release-branches.sh
@@ -7,16 +7,22 @@
 # ==-----------------------------------------------------------== #
 
 # Define target branch pattern
-TARGET_PATTERN="^release/.+$"
+STRICT_TARGET_PATTERN="^release/v[0-9]+\.[0-9]+\.[0-9]+$"
+LOOSE_TARGET_PATTERN="^release/v.+\..+\..+$"
 
 # Check if the pull request is targeting the branch
-if [[ "$GITHUB_BASE_REF" =~ $TARGET_PATTERN ]]; then
-    printf "Error: Pull requests to '%s' branches are not allowed.\n" "$GITHUB_BASE_REF"
-    exit 1
+if [[ "$GITHUB_BASE_REF" =~ $LOOSE_TARGET_PATTERN ]]; then
+    if ! [[ "$GITHUB_BASE_REF" =~ $STRICT_TARGET_PATTERN ]]; then
+        printf "Error: Pull request is targeting an invalid release branch name: '%s'\n" "$GITHUB_BASE_REF"
+        printf "Branch must follow SemVer format: release/v<MAJOR>.<MINOR>.<PATCH>\n"
+        exit 1
+    else
+        printf "Error: Pull requests to '%s' branches are not allowed.\n" "$GITHUB_BASE_REF"
+        exit 1
+    fi
 else
     # If not targeting the branch, skip the check
     echo "Pull request is not targeting a release branch; skipping the check."
 fi
 
 exit 0
-
--- tools/scripts/ci/protect-stable-branches.sh
+++ tools/scripts/ci/protect-stable-branches.sh
@@ -7,30 +7,28 @@
 # ==-----------------------------------------------------------== #
 
 # Define target branch pattern
-TARGET_PATTERN="^stable/.+$"
+STRICT_TARGET_PATTERN="^stable/v[0-9]+$"
+LOOSE_TARGET_PATTERN="^stable/v.+$"
 
-# Define allowed source branch prefixes
-ALLOWED_PREFIXES=("release/")
-
-# Convert array to regex pattern
-ALLOWED_PATTERN="^($(IFS='|'; echo "${ALLOWED_PREFIXES[*]}"))"
+# Define allowed source branch
+ALLOWED_PATTERN="^release/v[0-9]+\.[0-9]+\.[0-9]+$"
 
 # Check if the pull request is targeting the branch
-if [[ "$GITHUB_BASE_REF" =~ $TARGET_PATTERN ]]; then
+if [[ "$GITHUB_BASE_REF" =~ $LOOSE_TARGET_PATTERN ]]; then
+    if ! [[ "$GITHUB_BASE_REF" =~ $STRICT_TARGET_PATTERN ]]; then
+        printf "Error: Pull request is targeting an invalid stable branch name: '%s'\n" "$GITHUB_BASE_REF"
+        printf "Branch must follow SemVer format: stable/v<MAJOR>\n"
+        exit 1
     # Check if the source branch follows the allowed naming pattern
-    if ! [[ "$GITHUB_HEAD_REF" =~ $ALLOWED_PATTERN ]]; then
-        printf "Error: Pull requests to '%s' branches can only be from these branches:\n" "$GITHUB_BASE_REF"
-        for branch in "${ALLOWED_PREFIXES[@]}"; do
-            printf "   - %s\n" "$branch"
-        done
+    elif ! [[ "$GITHUB_HEAD_REF" =~ $ALLOWED_PATTERN ]]; then
+        printf "Error: Pull requests to '%s' can only be from release branches.\n" "$GITHUB_BASE_REF"
         exit 1
     else
         echo "Pull request to '$GITHUB_BASE_REF' from allowed branch '$GITHUB_HEAD_REF'."
     fi
 else
     # If not targeting the branch, skip the check
-    echo "Pull request is not targeting a release branch; skipping the check."
+    echo "Pull request is not targeting a stable branch; skipping the check."
 fi
 
 exit 0
-
